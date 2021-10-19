d7c5da551ed8bf2aefa4eb6681b76eb235f09809
tommy591 <tstephensonjr91@gmail.com>
2021-10-18 17:20:04 -0700

    adding new files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const express = require('express')
+const express = require('express');
+const path = require('path')
+const app = express()
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res) => {
+res.render( 'home' ) 
+})
+
+app.listen(3000, ()=> console.log('connected thomas'))
